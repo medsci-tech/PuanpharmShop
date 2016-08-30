ab2f3fdd3af7847a23fa22c86f491a769c95d8b5
wangcaiji <372117542@qq.com>
2016-08-30 13:31:43 +0800

    nn链接解密

--- app/Http/Middleware/MemberMiddleware.php
+++ app/Http/Middleware/MemberMiddleware.php
@@ -19,45 +19,45 @@ class MemberMiddleware
     public function handle($request, Closure $next)
     {
 		
-		// $aes = new Aes();
-		 // if (\Session::has('phone')) {
-            // return $next($request);
-        // } else {
-			
-			// $enphone = $aes->Decode(urldecode(Input::get('phone')),'n0u0norDi5k_maTe');
-			// $rules = ['phone'=>'required|digits:11'];
-			// $input = ['phone'=>$enphone];
-            // $validator = \Validator::make($input, $rules);
-			
-            // if ($validator->fails()) {
-                // return redirect('/member/notice');
-            // } else {
-                // if (!$member = Member::where('phone', $enphone)->first()) {
-                    // $member = new Member();
-                    // $member->phone = $enphone;
-                    // $member->save();
-                // }
-                // \Session::set('phone', $member->phone);
-                // return $next($request);
-            // }
-        // }
-		
-        if (\Session::has('phone')) {
+		$aes = new Aes();
+		 if (\Session::has('phone')) {
             return $next($request);
         } else {
-            $validator = \Validator::make($request->all(), ['phone' => 'required|digits:11']);
+			
+			$enphone = $aes->Decode(urldecode(Input::get('phone')),'n0u0norDi5k_maTe');
+			$rules = ['phone'=>'required|digits:11'];
+			$input = ['phone'=>$enphone];
+            $validator = \Validator::make($input, $rules);
+			
             if ($validator->fails()) {
                 return redirect('/member/notice');
             } else {
-                if (!$member = Member::where('phone', $request->input('phone'))->first()) {
+                if (!$member = Member::where('phone', $enphone)->first()) {
                     $member = new Member();
-                    $member->phone = $request->input('phone');
+                    $member->phone = $enphone;
                     $member->save();
                 }
                 \Session::set('phone', $member->phone);
                 return $next($request);
             }
         }
+		
+        // if (\Session::has('phone')) {
+            // return $next($request);
+        // } else {
+            // $validator = \Validator::make($request->all(), ['phone' => 'required|digits:11']);
+            // if ($validator->fails()) {
+                // return redirect('/member/notice');
+            // } else {
+                // if (!$member = Member::where('phone', $request->input('phone'))->first()) {
+                    // $member = new Member();
+                    // $member->phone = $request->input('phone');
+                    // $member->save();
+                // }
+                // \Session::set('phone', $member->phone);
+                // return $next($request);
+            // }
+        // }
 		
 		
 		
--- app/Http/Middleware/WxMiddleware.php
+++ app/Http/Middleware/WxMiddleware.php
@@ -19,42 +19,16 @@ class WxMiddleware
     public function handle($request, Closure $next)
     {   
 		
-		// $aes = new Aes();
+		$aes = new Aes();
 		
-		
-        // if (\Session::has('wx_member')) {
-            // return $next($request);
-        // } else {
-			// $enphone = $aes->Decode(urldecode(Input::get('phone')),'n0u0norDi5k_maTe');
-			// $rules = ['phone'=>'required|digits:11'];
-			// $input = ['phone'=>$enphone];
-            // $validator = \Validator::make($input, $rules);
-
-            // if ($validator->fails()) {
-				
-                // return redirect('/member/notice');
-            // } else {
-				
-                // $wechatUser = \Wechat::authorizeUser($request->fullUrl());
-                // $wxMember = WxMember::where('phone', $enphone)->where('openid', $wechatUser['openid'])->first();
-                // if ($wxMember) {
-                    // \Session::set('wx_member', $wxMember->id);
-                    // return $next($request);
-                // } else {
-                    // $wxMember = new WxMember();
-                    // $wxMember->phone = $enphone;
-                    // $wxMember->openid = $wechatUser['openid'];
-                    // $wxMember->save();
-                    // \Session::set('wx_member', $wxMember->id);
-                    // return $next($request);
-                // }
-            // }
-        // }
 		
         if (\Session::has('wx_member')) {
             return $next($request);
         } else {
-            $validator = \Validator::make($request->all(), ['phone' => 'required|digits:11']);
+			$enphone = $aes->Decode(urldecode(Input::get('phone')),'n0u0norDi5k_maTe');
+			$rules = ['phone'=>'required|digits:11'];
+			$input = ['phone'=>$enphone];
+            $validator = \Validator::make($input, $rules);
 
             if ($validator->fails()) {
 				
@@ -62,13 +36,13 @@ class WxMiddleware
             } else {
 				
                 $wechatUser = \Wechat::authorizeUser($request->fullUrl());
-                $wxMember = WxMember::where('phone', $request->input('phone'))->where('openid', $wechatUser['openid'])->first();
+                $wxMember = WxMember::where('phone', $enphone)->where('openid', $wechatUser['openid'])->first();
                 if ($wxMember) {
                     \Session::set('wx_member', $wxMember->id);
                     return $next($request);
                 } else {
                     $wxMember = new WxMember();
-                    $wxMember->phone = $request->input('phone');
+                    $wxMember->phone = $enphone;
                     $wxMember->openid = $wechatUser['openid'];
                     $wxMember->save();
                     \Session::set('wx_member', $wxMember->id);
@@ -76,6 +50,32 @@ class WxMiddleware
                 }
             }
         }
+		
+        // if (\Session::has('wx_member')) {
+            // return $next($request);
+        // } else {
+            // $validator = \Validator::make($request->all(), ['phone' => 'required|digits:11']);
+
+            // if ($validator->fails()) {
+				
+                // return redirect('/member/notice');
+            // } else {
+				
+                // $wechatUser = \Wechat::authorizeUser($request->fullUrl());
+                // $wxMember = WxMember::where('phone', $request->input('phone'))->where('openid', $wechatUser['openid'])->first();
+                // if ($wxMember) {
+                    // \Session::set('wx_member', $wxMember->id);
+                    // return $next($request);
+                // } else {
+                    // $wxMember = new WxMember();
+                    // $wxMember->phone = $request->input('phone');
+                    // $wxMember->openid = $wechatUser['openid'];
+                    // $wxMember->save();
+                    // \Session::set('wx_member', $wxMember->id);
+                    // return $next($request);
+                // }
+            // }
+        // }
 		
 		
     }
