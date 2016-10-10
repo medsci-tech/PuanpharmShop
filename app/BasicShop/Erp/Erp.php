b1bfafb02eddd0f95b795c54b3751151cabb2de3
Damon <whliujun@hotmail.com>
2016-10-10 15:10:00 +0800

    fix

--- app/BasicShop/Erp/Erp.php
+++ app/BasicShop/Erp/Erp.php
@@ -41,9 +41,40 @@ class Erp
         return $data;
     }
 
+    private function formatOrder($data)
+    {
+        $data['appkey'] = $this->appkey;
+        $data['sessionkey'] = $this->sessionkey;
+        $param = [
+            'json' => json_encode($data),
+            'secret' => $this->secret,
+        ];
+        $content = json_encode($param);
+        $request = [
+            'headers' => ['Content-Type: application/json'],
+            'url' => '106.75.134.39:9002/test/signJson',
+            'content' => $content
+        ];
+
+        $response = \HttpClient::post($request);
+        //dd($response);
+        //dd($response->json()->data->sign);
+        $data['sign'] = $response->json()->data->sign;
+        return $data;
+    }
+
     public function post($data) {
         $data = $this->format($data);
         $content = json_encode($data);
+        //dd($content);
+        $response = \HttpClient::post(['headers' => ['Content-Type: application/json'], 'url' => $this->url, 'content' => $content]);
+        return $response->json();
+    }
+
+    public function postOrder($data) {
+        $data = $this->formatOrder($data);
+        $content = json_encode($data);
+
         $response = \HttpClient::post(['headers' => ['Content-Type: application/json'], 'url' => $this->url, 'content' => $content]);
         return $response->json();
     }
