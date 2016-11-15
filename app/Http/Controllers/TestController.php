879077792ce55b5025c4435a2a77443ea9391a55
Damon <whliujun@hotmail.com>
2016-11-15 17:42:59 +0800

    fix

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Http\Requests;
+use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
@@ -74,126 +74,7 @@ class TestController extends Controller
 
     function test()
     {
-        $data = [
-            "method" => "gy.erp.trade.add",
-            "details" => [
-                [
-                    "qty" => 11,
-                    "item_code" => "4117",
-                    "price" => 11,
-                    "note" =>null,
-                    "refund" => 0,
-                    "oid" =>null,
-                    "sku_code" =>null,
-                ]
-            ],
-            "payments" => [
-                [
-                    "payment" => 100,
-                    "paytime" => "2016-09-08 09:54:22",
-                    "account" =>null,
-                    "pay_type_code" => "1012014021233754645",
-                    "pay_code" =>null
-                ]
-            ],
-            "invoices" => [
-                [
-                    "invoice_type" => 1,
-                    "invoice_title" =>null,
-                    "invoice_content" =>null,
-                    "invoice_amount" =>null,
-                    "bill_amount" =>null
-                ]
-            ],
-            "shop_code" => "易康伴侣",
-            "vip_code" => "18986109549",
-            "warehouse_code" => "易康伴侣",
-            "deal_datetime" => "2016-09-08 15:03:45",
-            "express_code" => "ZJS"
-        ];
 
-//        $data = [
-//            "method" => "gy.erp.trade.add",
-//            "format" =>null,
-//            "code" =>null,
-//            "qty" => 0,
-//            "amount" => 0,
-//            "payment" => 0,
-//            "cod" => false,
-//            "refund" => 0,
-//            "pay_datetime" => "2014-12-16 18:00:00",
-//            "tagId" =>null,
-//            "msg" =>null,
-//            "shop_code" => "易康伴侣",
-//            "platform_code" =>null,
-//            "vip_code" => "wangzgtest",
-//            "vip_name" => "wangzgtest",
-//            "unpaid_amount" => 0,
-//            "post_fee" => 0,
-//            "cod_fee" => 0,
-//            "discount_fee" => 0,
-//            "warehouse_code" => "A001",
-//            "express_code" => "ZJS",
-//            "plan_delivery_date" =>null,
-//            "business_man_code" =>null,
-//            "buyer_memo" =>null,
-//            "seller_memo" =>null,
-//            "receiver_name" =>null,
-//            "receiver_province" =>null,
-//            "receiver_city" =>null,
-//            "receiver_district" =>null,
-//            "receiver_phone" => "11111",
-//            "receiver_mobile" => "11111",
-//            "receiver_zip" =>null,
-//            "receiver_address" =>null,
-//            "area_id" =>null,
-//            "create_name" =>null,
-//            "unique_tid" =>null,
-//            "seller_memo_late" =>null,
-//            "deal_datetime" => "2014-12-16 18:00:00",
-//            "order_type_code" =>null,
-//            "order_settlement_code" =>null,
-//            "invoice_title" =>null,
-//            "platform_flag" => 0,
-//            "tag_code" =>null,
-//            "human_express" => false,
-//            "hold_info" =>null,
-//            "vipIdCard" =>null,
-//            "vipRealName" =>null,
-//            "vipEmail" =>null,
-//            "details" => [
-//                [
-//                    "qty" => 0,
-//                    "price" => 0,
-//                    "note" =>null,
-//                    "refund" => 0,
-//                    "oid" =>null,
-//                    "item_code" =>null,
-//                    "sku_code" =>null,
-//
-//                ]
-//            ],
-//            "payments" => [
-//                [
-//                    "payment" => 100,
-//                    "paytime" => "2014-12-17 09:54:22",
-//                    "account" =>null,
-//                    "pay_type_code" => "1012014021233754645",
-//                    "pay_code" =>null
-//                ]
-//            ],
-//            "invoices" => [
-//                [
-//                    "invoice_type" => 1,
-//                    "invoice_title" =>null,
-//                    "invoice_content" =>null,
-//                    "invoice_amount" =>null,
-//                    "bill_amount" =>null
-//                ]
-//            ]
-//        ];
-
-        dd(\Erp::postOrder($data));
     }
 
     function sendMessage() {
@@ -204,8 +85,8 @@ class TestController extends Controller
        \EMS::updatePrintData(1);
     }
 
-    function demo()
+    function demo(Request $request)
     {
-        dd(\Erp::addTrade());
+        dd(\Erp::addTrade($request->input('id')));
     }
 }
