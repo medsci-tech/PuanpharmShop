f86713d74aebace49ba4cbc48b3cc0e8a06fc444
fishyu <501254061@qq.com>
2017-06-12 16:01:16 +0800

    test

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -260,20 +260,20 @@
         });
         $('.footer').find('.menu a[href="' + window.location.pathname + '"]').parent().addClass('active');
     });
-    wx.onMenuShareAppMessage({
-        title: '易康伴侣', // 分享标题
-        desc: '商城', // 分享描述
-        link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-        imgUrl: '/shop/images/logo.jpg', // 分享图标
-        type: '', // 分享类型,music、video或link，不填默认为link
-        dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-        success: function () {
-            // 用户确认分享后执行的回调函数
-        },
-        cancel: function () {
-            // 用户取消分享后执行的回调函数
-        }
-    });
+//    wx.onMenuShareAppMessage({
+//        title: '易康伴侣', // 分享标题
+//        desc: '商城', // 分享描述
+//        link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+//        imgUrl: '/shop/images/logo.jpg', // 分享图标
+//        type: '', // 分享类型,music、video或link，不填默认为link
+//        dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+//        success: function () {
+//            // 用户确认分享后执行的回调函数
+//        },
+//        cancel: function () {
+//            // 用户取消分享后执行的回调函数
+//        }
+//    });
 </script>
 </body>
 </html>
